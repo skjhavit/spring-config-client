8c4d1fe27b9ad15fc92a651de865c2f4019efece
Sudhanshu Jha <sudhanshu_jha@crgl-thirdparty.com>
2020-06-11 22:32:26 -0500

    finalChange

--- src/main/java/com/example/springconfigclient/SpringConfigClientApplication.java
+++ src/main/java/com/example/springconfigclient/SpringConfigClientApplication.java
@@ -6,27 +6,33 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @SpringBootApplication
 public class SpringConfigClientApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(SpringConfigClientApplication.class, args);
 	}
 
+
 }
 
+@RefreshScope
 @RestController
 class MessageRestController {
 	@Autowired
 	private UserConfiguration userConfiguration;
-
 	@GetMapping("/userData")
 	public UserBean getUserData() {
-		System.out.println();
 		return new UserBean(userConfiguration.getName(), userConfiguration.getProfile(), userConfiguration.getMsg());
 	}
 }
